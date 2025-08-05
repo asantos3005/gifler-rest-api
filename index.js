822e02c2c69b53a8fef17316c73b42716fdd8541
Andrew Santos <andrewsan321@gmail.com>
2025-08-05 22:42:35 +1000

    Added cleanup for temp gif and also started setup for neon db postgresql

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 const express = require('express')
 const cors = require('cors');
+require('dotenv').config();
+const { Pool } = require('pg');
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+
+const pool = new Pool({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: {
+    require: true,
+  },
+});
+
+async function getPgVersion() {
+  const client = await pool.connect();
+  try {
+    const result = await client.query('SELECT version()');
+    console.log(result.rows[0]);
+  } finally {
+    client.release();
+  }
+}
+getPgVersion();
 
 const gifRoutes = require('./routes/gifRoutes')
 const app = express()
